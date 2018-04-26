c76c12368c6cbdfdb2b4a999d251121c8dff43aa
Paulus Esterhazy <pesterhazy@gmail.com>
2018-04-26 13:31:19 +0200

    WIP

---
+++ README.md
@@ -0,0 +1,18 @@
+# Using JS type hints
+
+Expected output
+
+```
+["ok" "ok"]
+```
+
+Actual output
+
+```
+demo
+[nil "ok"]
+demo2
+[nil "ok"]
+demo3
+[nil "ok"]
+```
--- src/my/core.cljs
+++ src/my/core.cljs
@@ -2,11 +2,22 @@
   (:require [goog.object]))
 
 (defn demo [obj]
-  (prn [:demo-obj
-        obj
-        (-> obj (.-very))
-        (some-> obj (.-very) (.-long))
+  (prn [(some-> obj (.-very) (.-long))
         (goog.object/getValueByKeys obj "very" "long")]))
 
-(let [obj #js {"very" #js {"long" "works"}}]
-  (demo obj))
+(defn demo2 [^js obj]
+  (prn [(some-> obj (.-very) (.-long))
+        (goog.object/getValueByKeys obj "very" "long")]))
+
+(defn demo3 [^js/Date obj]
+  (prn [(some-> obj (.-very) (.-long))
+        (goog.object/getValueByKeys obj "very" "long")]))
+
+(let [obj #js {"very" #js {"long" "ok"}}]
+  (prn obj)
+  (println "demo")
+  (demo obj)
+  (println "demo2")
+  (demo2 obj)
+  (println "demo3")
+  (demo3 obj))
